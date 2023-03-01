1058d054add0b695b997d6eb0935b27c52ccfc8f
Reorx <novoreorx@gmail.com>
2023-03-01 16:55:09 +0800

    update question 1 to use functions from utils

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo should be run in Python 3.8 or above.
--- question_1.py
+++ question_1.py
@@ -1,27 +1,11 @@
 import math
-from typing import TypeVar, Type, Callable
+from utils import create_row_formatter, number_input
+
 
 INTRO = """\
 The program prints the volume, surface area and circumference of a capsule having radius r ranging from 6 to N in increments of 6 and side length a\
 """
 
-N = TypeVar('N', int, float)
-
-
-def number_input(hint, number_type: Type[N], min=None) -> N:
-    try:
-        v = number_type(input(hint).strip())
-    except ValueError:
-        raise SystemExit('Please input a valid number')
-    if min is not None and v <= min:
-        raise SystemExit(f'Please input a number that is greater than {min}')
-    return v
-
-
-def create_row_formatter(column_widths: list[int]) -> Callable[[list], str]:
-    def formatter(values: list):
-        return ''.join(str(v).ljust(column_widths[i]) for i, v in enumerate(values))
-    return formatter
 
 def main():
     print(INTRO)
@@ -31,7 +15,7 @@ def main():
     side_length = number_input('Enter the length of side a: ', float, min=0)
 
     # print header
-    row_ = create_row_formatter([12, 16, 20, 13])
+    row_ = create_row_formatter([(12, 0), (16, 0), (20, 0), (13, 0)])
     header = row_(['Radius', 'Volume', 'Surface Area', 'Circumference'])
     print(header)
     print(len(header) * '-')
@@ -50,4 +34,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
