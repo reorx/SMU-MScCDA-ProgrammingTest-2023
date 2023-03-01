4a00af76974c4dfd968480eb6aba6a90ed8a718b
Reorx <novoreorx@gmail.com>
2023-03-01 16:51:42 +0800

    complete question 2

---
+++ question_2_test.py
@@ -0,0 +1,73 @@
+import unittest
+from question_2 import Project, analyse_projects
+
+
+def create_project(name, upfront_cost, rate_of_return_percent, duration_in_years, cash_io_of_years):
+    p = Project()
+    p.name = name
+    p.upfront_cost = upfront_cost
+    p.rate_of_return_percent = rate_of_return_percent
+    p.duration_in_years = duration_in_years
+    p.cash_io_of_years = cash_io_of_years
+    return p
+
+
+p_mars = create_project(
+    'Mars',
+    35000.0,
+    12.0,
+    3,
+    [10000.0, 27000.0, 19000.0],
+)
+
+
+p_inception = create_project(
+    'Inception',
+    35000.0,
+    12.0,
+    2,
+    [27000.0, 27000.0],
+)
+
+
+class TestQuestion2(unittest.TestCase):
+    def test_project_mars(self):
+
+        expected_output = """\
+                        Mars
+----------------------------------------------------------
+Year    |         Cash         |  PV Factor  |  Amount
+        |    Inflows/Outflows  |
+----------------------------------------------------------
+1       |      $10,000.00      |    0.8929   |  $8,928.57
+2       |      $27,000.00      |    0.7972   |  $21,524.23
+3       |      $19,000.00      |    0.7118   |  $13,523.82
+Total Income: $56,000.00
+Present Value of Future Benefits: $43,976.63
+Present Value of Future Costs: $35,000.00
+Net Present Value(NPV): $8,976.63"""
+
+        self.maxDiff = None
+        self.assertEqual(p_mars.calculate_result(), expected_output)
+
+    def test_project_inception(self):
+
+        expected_output = """\
+                        Inception
+----------------------------------------------------------
+Year    |         Cash         |  PV Factor  |  Amount
+        |    Inflows/Outflows  |
+----------------------------------------------------------
+1       |      $27,000.00      |    0.8929   |  $24,107.14
+2       |      $27,000.00      |    0.7972   |  $21,524.23
+Total Income: $54,000.00
+Present Value of Future Benefits: $45,631.38
+Present Value of Future Costs: $35,000.00
+Net Present Value(NPV): $10,631.38"""
+        self.maxDiff = None
+        self.assertEqual(p_inception.calculate_result(), expected_output)
+
+    def test_analyse_2_projects(self):
+        highest_income_p, highest_npv_p = analyse_projects([p_mars, p_inception])
+        self.assertEqual(highest_income_p.name, p_mars.name)
+        self.assertEqual(highest_npv_p.name, p_inception.name)
---
+++ utils.py
@@ -0,0 +1,65 @@
+from typing import TypeVar, Type, Callable
+
+
+def esc(*codes: int) -> str:
+    """Produces an ANSI escape code from a list of integers"""
+    return '\x1b[{}m'.format(';'.join(str(c) for c in codes))
+
+
+class ColorCode:
+    FG_GREEN = esc(32)
+    FG_END = esc(39)
+
+
+def colored_input(hint) -> str:
+    # add code for color start after the hint
+    v = input(hint + ColorCode.FG_GREEN)
+    # add code for color end after getting the input, without a newline
+    print(ColorCode.FG_END, end='')
+    # convert the input to the desired type
+    return v
+
+
+N = TypeVar('N', int, float)
+
+
+def number_input(hint, number_type: Type[N], min=None) -> N:
+    v = None
+    while v is None:
+        try:
+            v = number_type(colored_input(hint).strip())
+        except ValueError:
+            print('Please input a valid number')
+            continue
+        if min is not None and v < min:
+            print(f'Please input a number that is greater than or equal to {min}')
+            continue
+    return v
+
+
+def str_input(hint, min_length=1) -> str:
+    v = ''
+    while not v:
+        v = colored_input(hint).strip()
+        if len(v) < min_length:
+            print(f'Please input a string that is at least {min_length} characters long')
+            continue
+    return v
+
+
+def create_row_formatter(columns: list[tuple[int, int]], delimeter='') -> Callable[[list], str]:
+    """Create a formatter for rows in a table,
+    each column is defined by a tuple of (width, left_padding)
+    """
+    def formatter(values: list):
+        l = []
+        length = len(values)
+        for i, v in enumerate(values):
+            width, left_padding = columns[i]
+            text = f'{" " * left_padding}{v}'
+            # all text except the last one should be padded to the right
+            if i < length - 1:
+                text = text.ljust(width)
+            l.append(text)
+        return delimeter.join(l)
+    return formatter
